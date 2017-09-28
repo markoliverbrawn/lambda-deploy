a6660deed436b53744e9d25210a162a730efd8ca
Mark Oliver Brawn <mark@markoliverbrawn.com>
2017-09-29 10:53:40 +1300

    mocha

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
-var time = require('time');
 exports.handler = (event, context, callback) => {
 
 	// Some comment at 10:25
-
-    var currentTime = new time.Date();
-    currentTime.setTimezone("America/Los_Angeles");
     callback(null, {
         statusCode: '200',
-        body: 'The time in Los Angeles SQUIRREL is: ' + currentTime.toString(),
+        body: 'Hello on 20170929_1053',
     });
 };
