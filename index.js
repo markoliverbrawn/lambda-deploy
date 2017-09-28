2a4dfd15ba85e28cdbf9e2b9511ed37131c1d408
Mark Oliver Brawn <mark@markoliverbrawn.com>
2017-09-29 10:25:28 +1300

    template.json

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var time = require('time');
 exports.handler = (event, context, callback) => {
+
+	// Some comment at 10:25
+
     var currentTime = new time.Date();
     currentTime.setTimezone("America/Los_Angeles");
     callback(null, {
