45340f977d1a8c61634fa14da83c3d648d596a39
Mark Oliver Brawn <mark@markoliverbrawn.com>
2017-09-29 11:37:48 +1300

    mocha

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ exports.handler = (event, context, callback) => {
 	// Some comment at 10:25
     callback(null, {
         statusCode: '200',
-        body: 'Hello on 20170929_1053',
+        body: 'Hello on 20170929_1112',
     });
 };
